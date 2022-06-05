554f4e5ece8704d95434b7bf6f85aaafc36f0e5d
DevMohamedElshazly <moshazly416@gmail.com>
2022-06-05 19:36:53 +0200

    init-project

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== QUESTIONS ACCORDION ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+// Previously selected topic (if user selected)
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+// We validate if the user previously chose a topic
+
+// Activate / deactivate the theme manually with the button
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
